f5c4f452bbef6ece663a0a820e1e981557105f16
D M <danm0rboss@gmail.com>
2024-11-10 16:23:46 +0100

    bunny 2.0

--- main.py
+++ main.py
@@ -15,3 +15,30 @@ for i in range(1, 11):
 
 with open("colour.txt", "r+") as f:
     print(f.read())
+
+with open("horse.txt", "r+") as f:
+    print(f.read())
+
+rabbit = """
+            /|      __
+           / |   ,-~ /
+          Y :|  //  /
+          | jj /( .^
+          >-"~"-v"
+         /       Y
+        jo  o    |
+       ( ~T~     j
+        >._-' _./
+       /   "~"  |
+      Y     _,  |
+     /| ;-"~ _  l
+    / l/ ,-"~    \
+    \//\/      .- \
+     Y        /    Y    -Row
+     l       I     !
+     ]\      _\    /"\
+    (" ~----( ~   Y.  )
+~~~~~~~~~~~~~~~~~~~~~~~~~\\
+"""
+
+print(rabbit)
