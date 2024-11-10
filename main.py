9081553a4d0290981ea6332a20cb455a1a68b721
D M <danm0rboss@gmail.com>
2024-11-10 14:58:49 +0100

    greet user

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os
+from load_dotenv import load_dotenv
+
+load_dotenv()
+
+name = os.getenv("NAME")
+password = os.getenv("PASSWORD")
+
 print("Hellow, world")
+print(f"My name is {name}")
+print (f"My password is {password}")
